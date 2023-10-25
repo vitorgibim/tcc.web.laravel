3760a76ba35b58dd0b154486752a52823941c43a
Vitor Gibim <vitor.gibim@edu.unipar.br>
2023-10-24 23:19:48 -0300

    FIX school subjects

--- database/migrations/2023_10_11_112321_create_school_subjects_table.php
+++ database/migrations/2023_10_11_112321_create_school_subjects_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('school_subjects', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('workload', 50);
             $table->timestamps();
         });
     }
