f1f5d8c9d9e16f15728e0e2ff7a6c7d27a8f0cca
Vitor Gibim <vitor.gibim@edu.unipar.br>
2023-10-24 23:21:35 -0300

    FIX Courses

--- database/migrations/2023_10_11_112229_create_courses_table.php
+++ database/migrations/2023_10_11_112229_create_courses_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('description', 50);
             $table->timestamps();
         });
     }
