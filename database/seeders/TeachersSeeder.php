8dab42ac265f1c11508fc3369414cf70f75f6bff
Vitor Gibim <vitor.gibim@edu.unipar.br>
2023-10-27 09:26:50 -0300

    FIX Seeders

--- database/seeders/TeachersSeeder.php
+++ database/seeders/TeachersSeeder.php
@@ -3,24 +3,22 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Student;
+use App\Models\Teacher;
 use Illuminate\Support\Str;
 
-class StudentsTableSeeder extends Seeder
+class TeachersSeeder extends Seeder
 {
     public function run()
     {
         for ($i = 0; $i < 100; $i++) {
-            Student::create([
-                'name' => 'Student ' . $i,
-                'ra' => mt_rand(10000, 99999),
+            Teacher::create([
+                'name' => 'Teacher ' . $i,
                 'cpf' => mt_rand(100000000, 999999999),
                 'email' => strtolower(Str::random(10).'@email.com'),
                 'address' => 'Address ' . $i,
                 'address_number' => mt_rand(1, 1000),
                 'neighborhood' => 'Neighborhood ' . $i,
-                'city_id' => 1,
-                'course_id'=> 1
+                'city_id' => 1
             ]);
         }
     }
