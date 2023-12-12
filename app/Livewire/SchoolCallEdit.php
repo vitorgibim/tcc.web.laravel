b11bb6877d7b66ec74d1a6697a4f58612e6f2502
vitor gibim <vitor.gibim@edu.unipar.br>
2023-12-12 18:40:57 +0000

    FIX Edit Call

--- app/Livewire/SchoolCallEdit.php
+++ app/Livewire/SchoolCallEdit.php
@@ -15,7 +15,7 @@ class SchoolCallEdit extends Component
     public $id;
     public $name, $date, $teacher_id,$classroom_id, $school_subject_id, $status,$school_call, $student_school_call;
     public $teachers, $classrooms, $school_subjects, $students;
-    public $student_id, $student_id_has = [];
+    public $student_id=[], $student_id_has=[];
 
     public function mount($id){
         $this->id = $id;
